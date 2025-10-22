d4517549b0b0e444b6e5539f3b41c78255e53a29
jhennyf <jhennyfestudos2021@gmail.com>
2025-10-22 19:51:43 -0300

    adicionando controller e e view inicial

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,18 @@
+class MoviesController < ApplicationController
+  before_action :set_movie, only: %i[show]
+  def index
+    @movies = Movie.all
+  end
+
+  def show; end
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :synopsis, :release_year, :duration, :director, category_ids: [])
+  end
+end
